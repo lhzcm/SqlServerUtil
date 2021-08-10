6eb60c2ed348adcbf4f0959be4ce98708985d0be
lkl <lhzcm888888@gmail.com>
2021-08-10 17:39:31 +0800

    添加了创建表sql的方法

---
+++ src/p_if_create_table_sql.sql
@@ -0,0 +1,115 @@
+----------------------------------------------------------
+-- Procedure Name: cc p_if_create_table_sql, 0 ,1
+-- Author: Lkl
+-- Date Generated: 2021年08月10日
+-- Description: 获取创建table的sql
+----------------------------------------------------------
+create function p_if_create_table_sql(
+@tablename varchar(128)
+)
+returns @show table(text varchar(max))
+as
+begin
+declare @tbcolumn table(
+    rid int identity, 
+	object_id int,
+	column_name varchar(256), 
+	max_length int, 
+	precision int, 
+	scale int, 
+	is_nullable bit, 
+	system_type_id int, 
+	system_type_presc int,
+	system_type_scale int,
+	system_max_length int,
+	type_name varchar(128), 
+	collation_name varchar(128),
+	is_identity bit
+)
+declare @i int = 0
+declare @column_name varchar(256), 	
+@max_length int, 
+@precision int, 
+@scale int, 
+@is_nullable bit, 
+@system_type_id int, 
+@system_type_presc int,
+@system_type_scale int,
+@system_max_length int,
+@type_name varchar(128), 
+@collation_name varchar(128),
+@is_identity bit,
+@identity varchar(64) = '',
+@tb_object_id int
+
+declare @tb table(rid int identity, text varchar(max))
+declare @columntext varchar(1024) = ''
+
+--获取表的object_id
+select @tb_object_id = object_id from sys.tables where name = @tablename
+if @@rowcount <= 0
+    return
+--获取每一列的信息
+insert into @tbcolumn(object_id, column_name, max_length, precision, scale,
+is_nullable, system_type_id, system_type_presc, system_type_scale, system_max_length,
+type_name, collation_name, is_identity)
+select c.object_id, c.name, c.max_length, c.precision, c.scale,
+c.is_nullable, c.system_type_id, ty.precision, ty.scale, ty.max_length, 
+ty.name, c.collation_name, c.is_identity from sys.tables t 
+join sys.columns c on t.object_id = c.object_id 
+join sys.types ty on c.system_type_id = ty.system_type_id 
+where t.name = @tablename order by c.column_id asc
+
+--循环添加每一列的显示
+insert into @tb(text) select 'CREATE TABLE ' + @tablename + ' ('
+while 1 = 1
+begin
+    set @i += 1
+	select @column_name = column_name, @max_length = max_length, @precision = precision, @scale = scale, 
+	@is_nullable = is_nullable, @system_type_id = system_type_id, @system_type_presc = system_type_presc,
+	@system_type_scale = system_type_scale, @system_max_length = system_max_length, @type_name = type_name, 
+	@collation_name = collation_name, @is_identity = is_identity  from @tbcolumn where rid = @i
+	if @@ROWCOUNT <= 0
+	    break
+	--添加列名和类型
+	set @columntext = '    [' +  @column_name + '] ' + @type_name
+	if @precision != @system_type_presc or @scale != @system_type_scale
+	begin
+	    set @columntext += '(' + ltrim(@precision)
+		if @scale != @system_type_scale
+		    set @columntext += ',' + ltrim(@scale)
+	    set @columntext += ')'
+	end
+	else if @max_length != @system_max_length
+	begin
+	    set @columntext += '(' + ltrim(@max_length) + ')'
+	end
+	--添加是否不为空
+	if @is_nullable = 0
+	    set @columntext += ' not null'
+	--添加自增
+	if @is_identity = 1
+	begin
+	    declare @seed_value sql_variant = 0, @increment_value sql_variant = 0
+		select @seed_value = seed_value, @increment_value = increment_value from sys.identity_columns where object_id = @tb_object_id
+	    set @columntext += ' identity(' + convert(varchar, @seed_value) + ', ' + convert(varchar, @increment_value) + ')'
+	end
+	if exists(select 1 from @tbcolumn where rid = @i + 1) or exists(select 1 from sys.indexes where object_id = @tb_object_id and is_primary_key = 1)
+	    set @columntext += ','
+	insert into @tb(text) values(@columntext)
+end
+--添加主键
+declare @PK_name varchar(1024), @index_id int
+select @PK_name = name, @index_id = index_id from sys.indexes where object_id = @tb_object_id and is_primary_key = 1
+if @@rowcount > 0
+begin
+    set @columntext = '    constraint ' + @PK_name + ' primary key('
+	declare @keys varchar(1024) = isnull(rtrim((select col_name(object_id, column_id) + ', ' 
+	    from sys.index_columns where object_id = @tb_object_id and index_id = @index_id for xml path(''))), ' ')
+    set @columntext += substring(@keys, 1, len(@keys)-1) + ')'
+	insert into @tb(text) values(@columntext)
+end
+insert into @tb(text) select ')'
+insert @show(text) select text from @tb order by rid asc
+return
+end
