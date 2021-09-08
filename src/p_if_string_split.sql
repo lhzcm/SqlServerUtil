b426d3f070c4e396a87c4d77dffedf54cebe550f
lkl <lhzcm888888@gmail.com>
2021-09-08 11:20:48 +0800

    修复了对@separator末尾存在空格时的长度错误问题

--- src/p_if_string_split.sql
+++ src/p_if_string_split.sql
@@ -11,7 +11,8 @@ create function [dbo].[p_if_string_split](
 returns @tb table(rid int identity, result varchar(max))
 as
 begin
-    declare @len int = len(@separator)
+	--len函数会忽略文字后面空格，需要特殊处理一下
+	declare @len int = len(@separator) + (datalength(@separator) - datalength(rtrim(@separator)))    
 
 	declare @index int = 1
 	declare @tempindex int = 1
